a680236a40b8cbcc138f12703fdc3ac2d8eb2d09
Jaani Kekäläinen <64746435+jkeka@users.noreply.github.com>
2021-03-25 15:10:54 +0200

    Zoom debugging and fix

--- Hummingbird_Group1/Assets/Scripts/ZoomingPaningRotating.cs
+++ Hummingbird_Group1/Assets/Scripts/ZoomingPaningRotating.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZoomingPaningRotating : MonoBehaviour
 {
@@ -8,59 +9,84 @@ public class ZoomingPaningRotating : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float zoomOutMin = 1;
     [SerializeField] private float zoomOutMax = 8;
-    private Vector3 pervPos;
+    private Vector3 prevPos;
 
-    [SerializeField] private float slowDownTime = 0.01f;
+    public Text logText;
+    public Text diffText;
+
+    [SerializeField] private float slowDownTime = 0.005f;
 
     Vector3 dir;
 
     // Update is called once per frame
     void Update()
     {
-        
+
         Rotation();
+
+
+        //Two fingers on screen
         if (Input.touchCount == 2)
         {
+            logText.text = ("Two fingers on screen");
             Touch touchFirst = Input.GetTouch(0);
             Touch touchSecond = Input.GetTouch(1);
 
             Vector2 firstTouchPrevPos = touchFirst.position - touchFirst.deltaPosition;
             Vector2 secondTouchPrevPos = touchSecond.position - touchSecond.deltaPosition;
 
+            Debug.Log("touchFirstPos " + touchFirst.position);
+            Debug.Log("touchSecondPos " + touchSecond.position);
+
+
+
             float prevMagnitude = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
-            float currentMagnitude = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
+            float currentMagnitude = (touchFirst.position - touchSecond.position).magnitude;
+
+
+            Debug.Log("prevMagnitude " + prevMagnitude);
+            Debug.Log("currentMagnitude " + currentMagnitude);
+
 
             float difference = currentMagnitude - prevMagnitude;
             Zoom(difference * slowDownTime);
+            diffText.text = (difference.ToString());
 
         }
+        else
+        {
+            logText.text = ("No fingers");
+        }
+
+
+
     }
     void Rotation()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            pervPos = cam.ScreenToViewportPoint(Input.mousePosition);
+            prevPos = cam.ScreenToViewportPoint(Input.mousePosition);
         }
 
         if (Input.GetMouseButton(0))
         {
-            dir = pervPos - cam.ScreenToViewportPoint(Input.mousePosition);
+            dir = prevPos - cam.ScreenToViewportPoint(Input.mousePosition);
             cam.transform.position = target.position;
 
             cam.transform.Rotate(new Vector3(1, 0, 0) * dir.y * 180);
             cam.transform.Rotate(new Vector3(0, 1, 0) * -dir.x * 180, Space.World);
             cam.transform.Translate(new Vector3(0, 0, -10));
 
-            pervPos = cam.ScreenToViewportPoint(Input.mousePosition);
+            prevPos = cam.ScreenToViewportPoint(Input.mousePosition);
 
         }
       
        
 
     }
-    void Zoom(float diffrence)
+    void Zoom(float difference)
     {
-        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - diffrence, zoomOutMin, zoomOutMax);
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - difference, zoomOutMin, zoomOutMax);
 
     }
 }
