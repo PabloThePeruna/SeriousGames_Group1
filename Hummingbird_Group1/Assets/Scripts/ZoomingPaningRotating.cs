f1253cf4665f5ec0294a685a8142bf7bf407e55a
MiikkaKO99 <78470416+MiikkaKO99@users.noreply.github.com>
2021-03-25 13:21:32 +0200

    Added a zoominjg and rotating funtionality for testing
    code commented later

---
+++ Hummingbird_Group1/Assets/Scripts/ZoomingPaningRotating.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomingPaningRotating : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform target;
+    [SerializeField] private float zoomOutMin=1;
+    [SerializeField] private float zoomOutMax = 8;
+    private Vector3 pervPos;
+    float difference;
+    [SerializeField] private float slowDownTime = 0.01f;
+
+    Vector3 dir;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Zoom(difference* slowDownTime);
+        Rotation();
+    }
+    void Rotation()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pervPos = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            dir = pervPos - cam.ScreenToViewportPoint(Input.mousePosition);
+            cam.transform.position = target.position;
+
+            cam.transform.Rotate(new Vector3(1, 0, 0) * dir.y * 180);
+            cam.transform.Rotate(new Vector3(0, 1, 0) * -dir.x * 180, Space.World);
+            cam.transform.Translate(new Vector3(0, 0, -10));
+
+            pervPos = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+
+    }
+    void Zoom(float diffrence)
+    {
+        if(Input.touchCount== 2)
+        {
+            Touch touchFirst = Input.GetTouch(0);
+            Touch touchSecond = Input.GetTouch(1);
+
+            Vector2 firstTouchPrevPos = touchFirst.position - touchFirst.deltaPosition;
+            Vector2 secondTouchPrevPos = touchSecond.position - touchSecond.deltaPosition;
+
+            float prevMagnitude = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
+            float currentMagnitude = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
+
+            difference = currentMagnitude - prevMagnitude;
+
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - diffrence, zoomOutMin, zoomOutMax);
+        }
+    }
+    
+}
