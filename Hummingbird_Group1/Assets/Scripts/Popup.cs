0a898e3af0d63033f8d388289ae55402e19b0feb
Katariina <katariina.peltopihko@gmail.com>
2021-04-03 15:00:23 +0300

    Navigation + Popup
    Added first progress with navigation between scenes and popup window

---
+++ Hummingbird_Group1/Assets/Scripts/Popup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+
+
+public class Popup : MonoBehaviour
+{
+    public Button _button1;
+
+
+    public GameObject popup;
+
+
+    void Update()
+    {
+      _button1.onClick.AddListener(() =>
+        {
+            GameObject.Destroy(this.gameObject);
+        });
+            
+    }
+
+    
+}
+
---
+++ Hummingbird_Group1/Assets/Scripts/PopupController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupController : MonoBehaviour
+{
+    public static PopupController Instance;
+
+    void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; } // stops dups running
+        DontDestroyOnLoad(gameObject); // keep me forever
+        Instance = this; // set the reference to it
+
+    }
+
+    public Popup CreatePopup()
+    {
+        GameObject popUpGo = Instantiate(Resources.Load("UI/Popup") as GameObject);
+        return popUpGo.GetComponent<Popup>();
+    }
+}
