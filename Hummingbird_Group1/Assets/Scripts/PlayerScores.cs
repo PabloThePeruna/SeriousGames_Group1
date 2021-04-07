8a069bc4714a03596a1ef2a99aef34ff54cff6bd
MarkLugalia <77728724+MarkLugalia@users.noreply.github.com>
2021-04-07 09:11:58 +0300

    Added Database Updater
    Added an example scene which can place, retrieve and display randomly given "scores" set by "Users". Currently thinking about how to turn this example into a database handler that can hold our cases.

---
+++ Hummingbird_Group1/Assets/Scripts/PlayerScores.cs
@@ -0,0 +1,59 @@
+﻿using Proyecto26;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScores : MonoBehaviour
+{
+
+    public Text scoreText;
+    public InputField nameText;
+
+    User user = new User();
+
+    public static int playerScore;
+    public static string playerName;
+
+    private System.Random random = new System.Random();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScore = random.Next(0, 101);
+        scoreText.text = "Score " + playerScore;
+    }
+
+
+    public void OnSubmit()
+    {
+        playerName = nameText.text;
+        PostToDatabase();
+    }
+
+    public void OnGetScore()
+    {
+        RetrieveFromDatabase();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score " + user.userScore;
+    }
+
+    private void PostToDatabase()
+    {
+        User user = new User();
+        RestClient.Put("https://icarus-firebase-rdb-default-rtdb.firebaseio.com/" + playerName + ".json", user);
+    }
+
+    private void RetrieveFromDatabase()
+    {
+        RestClient.Get<User>("https://icarus-firebase-rdb-default-rtdb.firebaseio.com/" + nameText.text + ".json").Then ( response =>
+        {
+            user = response;
+            UpdateScore();
+        });
+    }
+}
---
+++ Hummingbird_Group1/Assets/Scripts/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class User
+{
+    public string userName;
+    public int userScore;
+
+    public User()
+    {
+        userName = PlayerScores.playerName;
+        userScore = PlayerScores.playerScore;
+    }
+
+
+}
