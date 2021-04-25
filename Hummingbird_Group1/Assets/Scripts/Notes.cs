c31d5c5e42a33be2c6b616cca8ba4962cb53b758
Katariina <katariina.peltopihko@gmail.com>
2021-04-25 13:30:44 +0300

    Moved to work in Case scene
    Working with note feature and making room for other to work with Both cases scene

---
+++ Hummingbird_Group1/Assets/Scripts/Notes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Notes : MonoBehaviour
+{
+    public GameObject Note;
+    public Button closeBtn;
+    public Button commentBtn;
+    private TMP_InputField noteInput; // Note input field
+       
+   
+    void awake()
+    {
+        
+    }
+
+    // Tähän tuplaklikkaus
+    void Update()
+    {
+        
+    }
+
+
+}
+
