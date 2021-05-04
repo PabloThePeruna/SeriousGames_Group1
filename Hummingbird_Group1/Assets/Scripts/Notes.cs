b4a08ba6887ce36cdcf1b35eb60b1918a73f321d
Katariina <katariina.peltopihko@gmail.com>
2021-05-04 11:17:08 +0300

    Added Note feature
    Note with scrollable chat box. Not sure is the double tapping working.

--- Hummingbird_Group1/Assets/Scripts/Notes.cs
+++ Hummingbird_Group1/Assets/Scripts/Notes.cs
@@ -3,29 +3,116 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class Notes : MonoBehaviour
 {
-    public GameObject Note;
-    public Button closeBtn;
+    public int maxMessages = 30; //Showing only 30 last messages
+
+    public GameObject chatPanel, textObject;
+    public TMP_InputField chatBox;
     public Button commentBtn;
-    private TMP_InputField noteInput; // Note input field
-       
-   
-    void awake()
+
+    // Set Note feature active by double tapping
+    public GameObject Note;
+    private float tapCount;
+         
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+
+    private void Start()
     {
-        
+        commentBtn.onClick.AddListener(Comment);
     }
 
-    // Tähän tuplaklikkaus
+    // Set Note feature active while double clicking the 2D collider
     void Update()
     {
-        
+
+       
+
+
     }
 
-    public void UpdateComments(string message)
+    //Create and add written note to messagelist
+    public void SendMessageToChat(string text)
     {
-        Debug.Log("Writing message!");
+        // Remove and Delete old messages
+        if (messageList.Count >= maxMessages)
+        {
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+
+        Message newMessage = new Message();
+
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+
+        newMessage.textObject = newText.GetComponent<Text>();
+
+        newMessage.textObject.text = newMessage.text;
+
+
+        messageList.Add(newMessage);
+
     }
+
+
+
+    // Clicking comment button send note to the chat
+    public void Comment()
+    {
+        if(chatBox.text != "")
+        {
+            
+            chatBox.ActivateInputField();
+            SendMessageToChat(">> " + chatBox.text);  //add mark before a note
+            chatBox.text = "";
+            
+        }
+    }
+
+    // Set a note active while double tapping
+    void DoubleTap()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            tapCount += 1;
+            StartCoroutine(Countdown()); // Start calculating is the tapping fast enpugh
+        }
+
+        if (tapCount == 2) //Double tapping
+        {
+            tapCount = 0;
+            StopCoroutine(Countdown());
+
+            Note.SetActive(true);   // Set Note to be seen
+        }
+
+    }
+
+    //For checking if tapping is fast enough
+    private IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(0.3f);
+        tapCount = 0;
+    }
+
+
+
+
+    [System.Serializable]
+    public class Message
+    {
+        public string text;
+        public Text textObject;
+
+
+    }
+
 }
+
 
